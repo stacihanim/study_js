ac062e62242fa9504cee56513046ea540d2aed58
stacyhanim <netrebenko4444@yahoo.com>
2023-02-22 17:42:48 +0200

    js intro

---
+++ script.js
@@ -0,0 +1,3 @@
+let firstNum = prompt('Enter the first number');
+let secondNum = prompt('Enter the second number');
+alert(`The result of addition is ${Number(firstNum) + Number(secondNum)}, subtraction ${Number(firstNum) - Number(secondNum)}, multiplication ${Number(firstNum) * Number(secondNum)} and division ${Number(firstNum) / Number(secondNum)}`);
