d9ccb7d70f9a532b8bb73022aa9e5075c22ef233
stacyhanim <netrebenko4444@yahoo.com>
2023-02-25 12:16:24 +0200

    now mathematical operators in separate windows

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const firstNum = prompt('Enter the first number');
 const secondNum = prompt('Enter the second number');
-alert(`The result of addition is ${Number(firstNum) + Number(secondNum)}, subtraction ${Number(firstNum) - Number(secondNum)}, multiplication ${Number(firstNum) * Number(secondNum)} and division ${Number(firstNum) / Number(secondNum)}`);
+alert(`The result of addition is ${Number(firstNum) + Number(secondNum)}`);
+alert(`The result of subtraction is ${Number(firstNum) - Number(secondNum)}`);
+alert(`The result of multiplication is ${Number(firstNum) * Number(secondNum)}`);
+alert(`The result of division is ${Number(firstNum) / Number(secondNum)}`);
+
