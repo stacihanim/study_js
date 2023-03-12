f03a2815bbdb7e8d1dc9c350d0111a04c16a7d4b
stacyhanim <netrebenko4444@yahoo.com>
2023-03-12 20:32:53 +0200

    working with functions

--- arrays.js
+++ arrays.js
@@ -1,28 +1,53 @@
 let arr = [ 7, 456, -5, 79, 23, 0, 12, 90, 65, 44, 2023];
-let sum = 0;
-let min = 0;
-let max = 0;
- 
-for(let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "number") {
-        sum += arr[i];
+
+function findMin(arr) {
+    let min = arr[0];
+    for(let i = 0; i < arr.length; i++) {
 
         if (min > arr[i]) {
             min = arr[i];
         }
+    }
+    console.log(min);
+}
+const minimum = findMin(arr);
+
+function findMax(arr) {
+    let max = arr[0];
+    for(let i = 0; i < arr.length; i++) {
 
         if (max < arr[i]) {
             max = arr[i];
-        }     
-    } 
-    
+        }
+    }
+    console.log(max);
 }
-console.log(sum);
-console.log(min);
-console.log(max);
+const maximum = findMax(arr);
 
-let hashtag = '';
-for (let i = 0; i < 5; i++) {
-console.log(hashtag+='#');
-}
+// let sum = 0;
+// let min = 0;
+// let max = 0;
+ 
+// for(let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] === "number") {
+//         sum += arr[i];
+
+//         if (min > arr[i]) {
+//             min = arr[i];
+//         }
+
+//         if (max < arr[i]) {
+//             max = arr[i];
+//         }     
+//     } 
+    
+// }
+// console.log(sum);
+// console.log(min);
+// console.log(max);
+
+// let hashtag = '';
+// for (let i = 0; i < 5; i++) {
+// console.log(hashtag+='#');
+// }
 
--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const firstNum = prompt('Enter the first number');
-const secondNum = prompt('Enter the second number');
-if (firstNum === "" || firstNum === null || secondNum === "" || secondNum === null) {
-    alert('Error');
-} else {
-    const addition = alert('Сума ' + (Number(firstNum) + Number(secondNum)));
-    if (Number(firstNum) < Number(secondNum)) confirm('Ви хочете продовжити операцію?') && alert('Різниця ' + (Number(firstNum) - Number(secondNum)));
-    const multiplication = alert('Добуток ' + Number(firstNum) * Number(secondNum));
-    const division = Number(secondNum) === 0 ? alert('На нуль ділити не можна') : alert('Частка ' + Number(firstNum) / Number(secondNum));    
+function add(num1, num2) {
+    if (typeof num1 === 'number' || typeof num2 === 'number') {
+        console.log('The result of addition is ' + (num1 + num2));
+    } 
 }
+
+function subtract(num1, num2) {
+    if (typeof num1 === 'number' || typeof num2 === 'number') {
+        console.log('The result of subtraction is ' + (num1 - num2));
+    } 
+}
+
+function multiply(num1, num2) {
+    if (typeof num1 === 'number' || typeof num2 === 'number') {
+        console.log('The result of multiplication is ' + num1 * num2);
+    } 
+}
+
+function divide(num1, num2) {
+    if (typeof num1 === 'number' || typeof num2 === 'number') {
+        console.log('The result of division is ' + num1 / num2);
+    } 
+}
+
+const addition = add(7, 8);
+const subtraction = subtract(8, 9);
+const multiplication = multiply(567, -23);
+const division = divide(92, 15);
+
+// const firstNum = prompt('Enter the first number');
+// const secondNum = prompt('Enter the second number');
+// if (firstNum === "" || firstNum === null || secondNum === "" || secondNum === null) {
+//     alert('Error');
+// } else {
+//     const addition = alert('Сума ' + (Number(firstNum) + Number(secondNum)));
+//     if (Number(firstNum) < Number(secondNum)) confirm('Ви хочете продовжити операцію?') && alert('Різниця ' + (Number(firstNum) - Number(secondNum)));
+//     const multiplication = alert('Добуток ' + Number(firstNum) * Number(secondNum));
+//     const division = Number(secondNum) === 0 ? alert('На нуль ділити не можна') : alert('Частка ' + Number(firstNum) / Number(secondNum));    
+// }
