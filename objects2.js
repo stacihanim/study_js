9634024a087b467650c72a3d568d070e4a1d92cb
stacyhanim <netrebenko4444@yahoo.com>
2023-03-26 09:17:11 +0300

    working with constructors

---
+++ objects2.js
@@ -0,0 +1,47 @@
+function Accumulator(num) {
+    this.num = num;
+
+    this.increment = function() {
+        this.num++;
+        console.log(this.num);
+    }
+    
+    this.decrement = function() {
+        this.num--;
+        console.log(this.num);
+    }
+
+
+    
+}
+Accumulator.prototype.increment = function() {
+    this.num++;
+    console.log(this.num);
+}
+Accumulator.prototype.decrement = function() {
+    this.num--;
+    console.log(this.num);
+}
+
+
+
+function CancelableAccumulator(num) {
+    Accumulator.call(this, num);
+    this.clear = function() {
+        this.num = num;
+        console.log(this.num);
+    };
+}
+
+CancelableAccumulator.prototype = Object.create(Accumulator);
+
+let accumulator = new CancelableAccumulator(1);
+accumulator.increment();
+accumulator.increment();
+accumulator.increment();
+accumulator.decrement();
+accumulator.increment();
+
+accumulator.clear();
+accumulator.increment();
+accumulator.decrement();
