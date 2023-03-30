e63925044cab40695c1ebcf087d0d4c89ea5eb3a
stacyhanim <netrebenko4444@yahoo.com>
2023-03-30 22:29:02 +0300

    DOM manipulations

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
+function ifNumber(num) {
+    if (typeof num !== 'number') {
+        console.error('Error, NaN');
+    } else {
+        return num;
+    }
+} 
+
+
 function add(num1, num2) {
-    if (typeof num1 === 'number' || typeof num2 === 'number') {
-        console.log('The result of addition is ' + (num1 + num2));
-    } 
+    return ifNumber(num1) + ifNumber(num2);
 }
 
 function subtract(num1, num2) {
-    if (typeof num1 === 'number' || typeof num2 === 'number') {
-        console.log('The result of subtraction is ' + (num1 - num2));
-    } 
+     return ifNumber(num1) - ifNumber(num2);
 }
 
 function multiply(num1, num2) {
-    if (typeof num1 === 'number' || typeof num2 === 'number') {
-        console.log('The result of multiplication is ' + num1 * num2);
-    } 
+     return ifNumber(num1) * ifNumber(num2);
 }
 
 function divide(num1, num2) {
-    if (typeof num1 === 'number' || typeof num2 === 'number') {
-        console.log('The result of division is ' + num1 / num2);
-    } 
+    return ifNumber(num1) / ifNumber(num2);
 }
 
-const addition = add(7, 8);
+
+function color(num) {
+    if (num < 0) {
+        num = 255 + num; 
+       return 'rgb(0,' + num + ', 0)';
+    } else {
+        num = 255 - num;
+        return 'rgb(' + num + ', 0, 0)';
+    }
+}
+
+const addition = add(7, 80);
 const subtraction = subtract(8, 9);
-const multiplication = multiply(567, -23);
+const multiplication = multiply(200, -1);
 const division = divide(92, 15);
+
+let one = document.getElementById('one');
+let one_t = document.getElementById('one_t');
+one_t.textContent = 'The result of addition is ';
+one.textContent = addition;
+one.style.color = color(addition);
+
+let two = document.getElementById('two');
+let two_t = document.getElementById('two_t');
+two_t.textContent = 'The result of subtraction is ';
+two.textContent = subtraction;
+two.style.color = color(subtraction);
+
+let three = document.getElementById('three');
+let three_t = document.getElementById('three_t');
+three_t.textContent = 'The result of multiplication is ';
+three.textContent = multiplication;
+three.style.color = color(multiplication);
+
+let four = document.getElementById('four');
+let four_t = document.getElementById('four_t');
+four_t.textContent = 'The result of division is ';
+four.textContent = division;
+four.style.color = color(division);
+
+/* Previous code */
 
 // const firstNum = prompt('Enter the first number');
 // const secondNum = prompt('Enter the second number');
