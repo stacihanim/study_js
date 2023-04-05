861f4de114e697ee1b82a22431f05c237326394c
stacyhanim <netrebenko4444@yahoo.com>
2023-04-05 17:36:05 +0300

    fixing errors

---
+++ errors.js
@@ -0,0 +1,47 @@
+// 1
+
+function calculateAverage(arr) {
+    let sum = 0;
+    let avg = 0;
+    for (let i = 0; i <= arr.length; i++) {
+      sum += i;
+      avg = sum / arr.length;
+     
+    }
+
+    return avg;
+  }
+  
+  let numbers = [1, 2, 3, 4, 5];
+  
+  let avg = calculateAverage(numbers);
+  console.log("The average is: " + avg); // має бути 3
+
+
+
+  // 2
+function User(name, age) {
+	this.name = name;
+    this.age = age;
+}
+
+User.prototype.sayHello = function() {
+	return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+}
+
+const user = new User('Sherlock', 27);
+console.log(user.sayHello()); // має вивести "Hi. My name is Sherlock and I'm 27 years old"
+
+
+
+
+// 3
+const calculator = function(init = 1) {
+	return function () {
+  	return init++;
+  }
+}
+const c = calculator();
+console.log(c()); // має вивести 1
+console.log(c()); // має вивести 2
+
